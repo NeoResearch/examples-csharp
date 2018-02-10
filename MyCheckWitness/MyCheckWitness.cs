6fe8065b016d0567897185dea591f1845a047100
Igor Machado <igor.machado@gmail.com>
2018-02-10 21:08:41 -0200

    three options for address initialization

--- MyCheckWitness/MyCheckWitness.cs
+++ MyCheckWitness/MyCheckWitness.cs
@@ -5,9 +5,9 @@ namespace Neo.SmartContract
 {
     public class MyCheckWitness : Framework.SmartContract
     {
-        //Token Settings
         public static readonly byte[] Owner = "AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y".ToScriptHash();
-        public static readonly byte[] Owner2 = "031a6c6fbbdf02ca351745fa86b9ba5a9452d785ac4f7fc2b7548ca2a46c4fcf4a".HexToBytes();
+        //public static readonly byte[] Owner = "031a6c6fbbdf02ca351745fa86b9ba5a9452d785ac4f7fc2b7548ca2a46c4fcf4a".HexToBytes();
+        //public static readonly byte[] Owner = {3,26,108,111,187,223,2,202,53,23,69,250,134,185,186,90,148,82,215,133,172,79,127,194,183,84,140,162,164,108,79,207,74};
 
         public static bool Main()
         {
@@ -21,4 +21,3 @@ namespace Neo.SmartContract
         }
     }
 }
-
